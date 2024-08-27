f7a1837ecc6166cfea2fd84d0e14795a0581f1a3
davidebenassi <davideben2002@gmail.com>
2024-08-27 23:49:33 +0200

    ignore static & UserPrifile model

--- project-folder/users/models.py
+++ project-folder/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+   
+    picture = models.ImageField(
+        upload_to='static/users_profile_pictures/',
+        default='static/images/blank_profile_picture.jpg',
+        blank=True
+    )
+    #tickets = models.ManyToManyField(to = '', through = '', blank = True, related_name = 'userTickets')
+
+    def __str__(self):
+      return f'{self.user.first_name} {self.user.last_name} - {self.user.username}'
