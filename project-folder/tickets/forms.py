590313c22ec38182880c2dc312fefd511221d2a8
davidebenassi <davideben2002@gmail.com>
2024-08-29 23:52:08 +0200

    started creating tickets logic

---
+++ project-folder/tickets/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Ticket
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description']
+        
+        widgets = {
+            'title': forms.TextInput(),
+            'description': forms.Textarea(),
+        }
+
+
+'''
+'title': forms.TextInput(attrs={'class': 'form-control'}),
+'description': forms.Textarea(attrs={'class': 'form-control'}),
+'''
--- project-folder/tickets/models.py
+++ project-folder/tickets/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    OPEN = 'open'
+    CLOSED = 'closed'
+    PENDING = 'pending'
+
+    STATUS_CHOICES = [
+        (OPEN, 'Open'),
+        (CLOSED, 'Closed'),
+        (PENDING, 'Pending')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='created_tickets')
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='tickets')
+    assigned_employee = models.ForeignKey('EmployeeProfile', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Ticket #{self.id} - {self.title} ({self.status})"
