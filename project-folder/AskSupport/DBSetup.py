d373e6ddad470406a2cd2966f5b761a25947b9b9
davidebenassi <davideben2002@gmail.com>
2024-09-05 14:13:56 +0200

    setupdb

--- project-folder/AskSupport/DBSetup.py
+++ project-folder/AskSupport/DBSetup.py
@@ -1,16 +1,19 @@
 from companies.models import Company, EmployeeProfile
 from tickets.models import Ticket, Message
 from users.models import UserProfile
+from faq.models import FAQ
 from django.contrib.auth.models import User, Group
 from AskSupport.settings import STATICFILES_DIRS
 import os
 import json
-from django.db import transaction, connection
+from django.db import connection
+import random
+from django.apps import apps
 
 ADMIN_USERNAME = "admin"
 ADMIN_PWD = "admin"
 
-def reset_ids(tables):
+def reset_tables_ids(tables):
 	for t in tables:
 		with connection.cursor() as cursor:
 			cursor.execute(f"DELETE FROM sqlite_sequence WHERE name = '{t}';")
@@ -49,13 +52,16 @@ def eraseDB():
 
 def initDB():
 
+    tables = ['auth_user', 'companies_company', 'companies_employeeprofile', 'users_userprofile', 'tickets_ticket', 'tickets_message', 'faq_faq']
+    reset_tables_ids(tables)
+
     admin = User.objects.create_superuser(username=ADMIN_USERNAME, password=ADMIN_PWD)
     admin.save()
     print(f"Superuser Created with username : {ADMIN_USERNAME}  -  password : {ADMIN_PWD}")
 
-    # Creazione Utenti #
-    usersJsonFile = os.path.join(os.path.dirname(__file__), 'DBSetupJson', 'users.json')
-    with open(usersJsonFile, 'r') as file:
+    ## * Creazione Utenti * ##
+    jsonFile = os.path.join(os.path.dirname(__file__), 'DBSetupJson', 'users.json')
+    with open(jsonFile, 'r') as file:
         data = json.load(file)
 
     for item in data:
@@ -72,23 +78,101 @@ def initDB():
         userProfile.user = user
         userProfile.save()
 
-    ## Crea azienda 1 #
-    # Admin
-    admin = User.objects.create_user(
-            username = 'adminazienda',
-            password = 'PSicura1!',
-            email = 'admin@azienda.it',
-            first_name = 'Luca',
-            last_name = 'Valori'
-        )
-    Group.objects.get(name='CompanyAdministrators').user_set.add(admin)
-    admin.save()
-    # Company
-    c = Company()
-    c.name = 'Azienda 1'
-    c.description = "Azienda tech"
-    c.admin = admin
-    c.save()
-
-
+    ## * Creazione Aziende * ##
+    jsonFile = os.path.join(os.path.dirname(__file__), 'DBSetupJson', 'companies.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+    
+    for item in data:
+        admin = User.objects.create_user(
+                username = item['admin']['username'],
+                password = item['admin']['password'],
+                email = item['admin']['email'],
+                first_name = item['admin']['first_name'],
+                last_name = item['admin']['last_name']
+            )
+        Group.objects.get(name='CompanyAdministrators').user_set.add(admin)
+        admin.save()
         
+        c = Company()
+        c.name = item['company']['name']
+        c.description = item['company']['description']
+        c.admin = admin
+        c.save()
+
+
+    ## * Get saved companies * ##
+    companies = list(Company.objects.all())
+
+    ## * Creazione Employee * ##
+
+    jsonFile = os.path.join(os.path.dirname(__file__), 'DBSetupJson', 'employees.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+
+    for item in data:
+        emp = User.objects.create_user(
+                username = item['username'],
+                password = item['password'],
+                email = item['email'],
+                first_name = item['first_name'],
+                last_name = item['last_name']
+            )
+        
+        Group.objects.get(name='Employees').user_set.add(emp)
+        emp.save()
+
+        empProfile = EmployeeProfile()
+        empProfile.user = emp
+        empProfile.company = random.choice(companies[:3]) # Leave a company without employees #   
+        empProfile.save()
+
+    ## * Creazione FAQs * ##
+
+    jsonFile = os.path.join(os.path.dirname(__file__), 'DBSetupJson', 'faqs.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+
+    for item in data[:4]:
+        faq = FAQ()
+        faq.question = item['question']
+        faq.answer = item['answer']
+        faq.approved = item['approved']
+        faq.company = companies[0] 
+        faq.save()     
+
+    for item in data[4:7]:
+        faq = FAQ()
+        faq.question = item['question']
+        faq.answer = item['answer']
+        faq.approved = item['approved']
+        faq.company = companies[1] 
+        faq.save()   
+
+    for item in data[7:10]:
+        faq = FAQ()
+        faq.question = item['question']
+        faq.answer = item['answer']
+        faq.approved = item['approved']
+        faq.company = companies[1] 
+        faq.save() 
+
+    print("DB Setup Completed")
+    print()
+
+    print('Users')
+    for u in list(UserProfile.objects.all()):
+        print('\t', u)
+
+    print()
+    print('Companies')
+    for c in companies:
+        print('\t',c)
+
+    print()
+    print('Employees')
+    for e in list(EmployeeProfile.objects.all()):
+        print('\t', e)
+
+    print()
+    print("Created 10 FAQs")
